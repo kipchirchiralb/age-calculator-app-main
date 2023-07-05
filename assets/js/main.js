a37939243a6a7ae49872ba54a29d3e45171bb6ee
albert <kipchirchiralb@gmail.com>
2023-07-05 11:58:23 +0300

    moment js included and age gap calculate

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,54 @@ const yearEl = document.getElementById("year");
 
 const today = new Date(); // current date
 
+function validateDay(day, element, max) {
+  let pError = document.createElement("p");
+  if (day < 1 || day > max) {
+    pError.textContent = "Must be a valid date";
+    pError.setAttribute("id", "day-error");
+    if (!document.getElementById("day-error")) {
+      element.parentElement.append(pError);
+    }
+  } else {
+    if (document.getElementById("day-error")) {
+      document.getElementById("day-error").remove();
+    }
+  }
+}
+
 dayEl.addEventListener("keyup", (e) => {
-  console.log(e.target.value);
+  if (e.target.value.length > 2) {
+    e.target.value = e.target.value.substr(0, 2);
+  }
+  validateDay(e.target.value, e.target, 31);
 });
+
 monthEl.addEventListener("keyup", (e) => {
+  if (e.target.value.length > 2) {
+    e.target.value = e.target.value.substr(0, 2);
+  }
+  // feb days error
+  if (e.target.value == 2 && document.getElementById("day").value > 28) {
+    console.log("feb condition");
+    validateDay(document.getElementById("day").value, dayEl, 28);
+  } else {
+    validateDay(document.getElementById("day").value, dayEl, 31);
+  }
+
+  // months ending day 30
+  if (
+    (e.target.value == 4 ||
+      e.target.value == 6 ||
+      e.target.value == 9 ||
+      e.target.value == 11) &&
+    document.getElementById("day").value > 30
+  ) {
+    console.log("30 days month condition");
+    validateDay(document.getElementById("day").value, dayEl, 30);
+  } else {
+    validateDay(document.getElementById("day").value, dayEl, 31);
+  }
+
   if (e.target.value < 1 || e.target.value > 12) {
     let pError = document.createElement("p");
     pError.textContent = "Must be a valid month";
@@ -52,3 +96,47 @@ yearEl.addEventListener("keyup", (e) => {
     }
   }
 });
+
+document.getElementById("get-result").addEventListener("click", () => {
+  let daySelected = document.getElementById("day").value;
+  let monthSelected = document.getElementById("month").value;
+  let yearSelected = document.getElementById("year").value;
+
+  if (
+    daySelected.length > 0 &&
+    yearSelected.length > 0 &&
+    monthSelected.length > 0 &&
+    !checkForErrors()
+  ) {
+    console.log("proceed!! proper dates and no errors");
+    let selectedDate = new Date(yearSelected, monthSelected - 1, daySelected);
+    let moment1 = moment(today.toLocaleString());
+    let moment2 = moment(selectedDate.toLocaleString());
+    let diffDuration = moment.duration(moment1.diff(moment2));
+    let years = diffDuration.years();
+    let months = diffDuration.months();
+    let days = diffDuration.days();
+    console.log(years + " Years. " + months + " Months. " + days + " Days. ");
+    document.getElementById("years").textContent = years;
+    document.getElementById("months").textContent = months;
+    document.getElementById("days").textContent = days;
+  } else {
+    console.log("improper dates or errors"); // fields connot be empty and there should be no no errors
+  }
+});
+
+function checkForErrors() {
+  let pCount = 0;
+  document.querySelectorAll(".input").forEach((el) => {
+    if (
+      Array.prototype.slice.call(el.children).some((el) => el.tagName === "P")
+    ) {
+      pCount++;
+    }
+  });
+  if (pCount > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
